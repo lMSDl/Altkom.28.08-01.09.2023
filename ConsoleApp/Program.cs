bf058320f8d6fd61972c17eaff9cb9f0d9df906c
Paweł Łęczycki <pawel@msd.info.pl>
2023-08-28 09:39:57 +0200

    Wprowadzenie do .NET 6

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+//namespace ConsoleApp
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+
+System.Console.WriteLine("Hello, World!");
+JsonConvert.SerializeObject(new object());
+
+// ? - (jawnie) nullowalny string
+string? a = null;
+string b = ToUpper(a!);
+
+Console.WriteLine(b);
+
+string ToUpper(string a /*!! - null-checking feature - dodaje podczas kompilacji (niejawnie) kod wyjątku jako poniżej*/)
+{
+    if (a == null)
+        throw new ArgumentNullException(nameof(a));
+
+    return a.ToUpper();
+}
+
+
+//        }
+//    }
+//}
