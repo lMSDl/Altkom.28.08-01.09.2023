b79b491182c343736ba04babf8edbd97ef720f16
Paweł Łęczycki <pawel@msd.info.pl>
2023-08-29 10:44:10 +0200

    Budowa aplikacji

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -7,6 +7,70 @@ var app = builder.Build();
 
 //app.Services.GetService....
 
+
+app.Use(async (context, next) =>
+{
+    await Console.Out.WriteLineAsync("Begin USE1");
+    await next(context);
+    await Console.Out.WriteLineAsync("End USE1");
+
+});
+
+
+app.Map("/Tom", mapApp =>
+{
+    mapApp.Use(async (context, next) =>
+    {
+        await Console.Out.WriteLineAsync("Begin MapUSE");
+        await next(context);
+        await Console.Out.WriteLineAsync("End MapUSE");
+
+    });
+
+    mapApp.Run(async context =>
+    {
+        await Console.Out.WriteLineAsync("Begin MapRUN");
+        await context.Response.WriteAsync("Hello Tom");
+        await Console.Out.WriteLineAsync("End MapRUN");
+    });
+});
+
+app.MapWhen(context => context.Request.Query.TryGetValue("name", out _), mapApp =>
+{
+    mapApp.Run(async context =>
+    {
+        await Console.Out.WriteLineAsync("Begin MapRUN");
+        await context.Response.WriteAsync($"Hello {context.Request.Query["name"]}");
+        await Console.Out.WriteLineAsync("End MapRUN");
+    });
+});
+
+
+app.Use(async (context, next) =>
+{
+    await Console.Out.WriteLineAsync("Begin USE2");
+    await next(context);
+    await Console.Out.WriteLineAsync("End USE2");
+
+});
+
+app.Run(async context =>
+{
+    await Console.Out.WriteLineAsync("Begin RUN");
+    await context.Response.WriteAsync("Hello in main Run");
+    await Console.Out.WriteLineAsync("End RUN");
+});
+
+
+
+
+
+
+
+
+
+
+
 //if (app.Environment.IsDevelopment())
 //{
 //    app.MapGet("/", () => "Hello World! D");
