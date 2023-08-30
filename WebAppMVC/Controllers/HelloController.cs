860382f98433a2e38fcad5f32244140ecae3fc68
Paweł Łęczycki <pawel@msd.info.pl>
2023-08-30 09:51:37 +0200

    Controller

---
+++ WebAppMVC/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace WebAppMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello world", "text/html");
+        }
+
+        //nazwa parametru == nazwa parametru w routingu => wartosc pobierana z path
+        //nazwa parametru != nazwa z routingu => wartość pobierana z query
+        public IActionResult Hi(string name, int age)
+        {
+            return Content($"Hello {name}. Your age is {age}", "text/html");
+        }
+
+        public IActionResult Encode(string name)
+        {
+            return Content(HttpUtility.HtmlEncode($"<b>Hello</b> {name}"), "text/html");
+        }
+    }
+}
