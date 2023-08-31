9c39279bcb7aa0e1465d83e28336e2f051463589
Paweł Łęczycki <pawel@msd.info.pl>
2023-08-31 08:33:37 +0200

    Formularz - wyszukiwanie

--- WebAppMVC/Controllers/UsersController.cs
+++ WebAppMVC/Controllers/UsersController.cs
@@ -17,5 +17,19 @@ namespace WebAppMVC.Controllers
 		{
 			return View(await _service.ReadAsync());
 		}
+
+		public async Task<IActionResult> Search(string? phrase)
+		{
+			var items = await _service.ReadAsync();
+
+			if (!string.IsNullOrWhiteSpace(phrase))
+			{
+				var properties = typeof(User).GetProperties().Where(x => x.CanRead).ToList();
+				items = items.Where(item => properties.Any(property =>
+					property.GetValue(item)?.ToString()?.Contains(phrase, StringComparison.InvariantCultureIgnoreCase) ?? false)).ToList();
+			}
+
+			return View(nameof(Index), items);
+		}
 	}
 }
