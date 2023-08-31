7970a79dadcdbeb26e0912d7e65fa862a8e371b7
Paweł Łęczycki <pawel@msd.info.pl>
2023-08-31 11:50:24 +0200

    Edycja

--- WebAppMVC/Controllers/UsersController.cs
+++ WebAppMVC/Controllers/UsersController.cs
@@ -16,7 +16,7 @@ namespace WebAppMVC.Controllers
 
 		public async Task<IActionResult> Index()
 		{
-			return View(await _service.ReadAsync());
+			return View((await _service.ReadAsync()).OrderBy(x => x.Id));
 		}
 
 		//[ValidateAntiForgeryToken]
@@ -45,6 +45,26 @@ namespace WebAppMVC.Controllers
 		public async Task<IActionResult> DeleteUser(int id)
 		{
 			await _service.DeleteAsync(id);
+
+			return RedirectToAction(nameof(Index));
+		}
+
+		public async Task<IActionResult> Edit(int id)
+		{
+			var user = await _service.ReadAsync(id);
+			return View(user);
+		}
+
+		//public async Task<IActionResult> EditUser(int id, string userName, string password)
+		//public async Task<IActionResult> EditUser(User editedUser)
+		public async Task<IActionResult> EditUser(int id, [Bind("UserName", "Password")]User editedUser)
+		{
+			var user = await _service.ReadAsync(id);
+
+			user.UserName = editedUser.UserName;
+			user.Password = editedUser.Password;
+
+			await _service.UpdateAsync(id, user);
 
 			return RedirectToAction(nameof(Index));
 		}
